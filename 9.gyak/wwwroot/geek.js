ec833416ed52a565089b8f389d98de62df3ab589
p4lc5f <p4lc5f@SZA0030898>
2022-11-23 15:21:31 +0100

    10.gyak

---
+++ 9.gyak/wwwroot/geek.js
@@ -0,0 +1,24 @@
+﻿window.onload = ()=> {
+    console.log("Oldal betöltve...");
+}
+var viccek;
+
+var letöltés=function () {
+    fetch('/jokes.json')
+        .then(x=>x.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+     console.log("Sikeres letöltés")
+     console.log(d)
+     viccek = d;
+
+    for (var i = 0; i < viccek.length; ++i) {
+        console.log(viccek[i].text);
+        let elem = document.createElement("div");
+        elem.innerHTML = viccek[i].text;
+        ide.appendChild(elem);
+    }
+}
---
+++ 9.gyak/wwwroot/jsgyak.js
@@ -0,0 +1,27 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+}
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+function fck() {
+    for (var sor = 0; sor < 10; sor++) {
+        var uj = document.createElement("div");
+        uj.classList.add("sor");
+        document.getElementById("Pascal").appendChild(uj);
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var nyuv= document.createElement("div");
+            nyuv.classList.add("elem");
+            nyuv.innerHTML = (faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor - oszlop)));
+            uj.appendChild(nyuv);
+        }
+    }
+}
+
+fck();
