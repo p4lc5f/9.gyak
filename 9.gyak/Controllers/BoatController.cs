5453d2d6f95cfefea4bef741fdb7a837596d49b2
p4lc5f <p4lc5f@SZA0030898>
2022-11-30 14:56:34 +0100

    kristof egy fasz

---
+++ 9.gyak/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _9.gyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
---
+++ 9.gyak/Controllers/TesztController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _9.gyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return Ok(pontosIdő);
+        }
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+            return BadRequest("Nem jó a bemenő adat");
+        }
+    }
+}
